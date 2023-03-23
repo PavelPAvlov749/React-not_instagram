6adc09bf44f679ed610e267371c840591a77dcf6
Pavel <81172841+PavelPAvlov749@users.noreply.github.com>
2023-03-24 04:12:28 +0600

    Add Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//Styles module import
+import styles from "../src/App.module.css"
+//React and readt hooks imports
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+//TSXComponent imports
+import { Router } from './Router/Router';
+import { Navbar } from './Components/Navbar/Navbar';
+import { Preloader } from './Components/Preloader/Preloader';
+//Redux,Actions and Thnunks imports
+import { postActions } from './Redux/PostReducer';
+import { connect } from 'react-redux';
+import { Global_state_type } from './Redux/Store';
+import { InitializeThunk } from './Redux/AppReducer';
+//Types imports
+import { AppPropsType } from './Redux/Types';
+//Firebase imports
+import { ref,getDatabase, onChildAdded, onChildChanged } from "firebase/database";
+//Media and assets
+const sound = require("../src/Media/MessageTone.mp3")
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+//Main component
+//This component renders the entire main layout of the application and calls the application initialization function in useEffect hook.
+//The App passes the props.isAuth key to the Router component to check the user's authorization status if the key is false, 
+//the Router will redirect the user to the login page.
+//<props> : 
+//isAuth :  userAutorization status
+//isInit : is Application initialized (if false App function will render Preloader component untill props.isInit becomes true value )
+//currentUserID : ID of current authorizated user
+//isFech : URL request flag indicator of the absence of current network requests aslo if false App will render Preloader
+//props.init() : function initializer using InitializeThunk function witch comes from ../Redux/AppReducer.ts
+
+const App: React.FC<AppPropsType> = React.memo((props: AppPropsType) => {
+  //Intialize App
+  useEffect( () => {
+    props.init()
+  }, [])
+  const [onMessage,setOnMessage] = useState(false)
+
+  const db = getDatabase();
+  const messageRef = ref(db,("Chat/"))
+  onChildChanged(messageRef,(message) => {
+    setOnMessage(true)
+    console.log("Message recieved")
+  })
+  //If one of them fasle return Preloader anotherwise return router
+  if (props.isInit || !props.isFetch) {
+    return (
+      <div className={styles.app}>
+          <HashRouter >
+          {onMessage ? <audio src={sound} autoPlay={true}></audio> : null}
+          <Navbar />
+            <Router actualUser={props.currentUserID as string} isAuth={props.isAuth} />
+        
+          </HashRouter  >
+
+      </div>
+    )
+  } else {
+    return (
+      <div>
+          <HashRouter >
+            <Preloader />
+            <Navbar />
+          </HashRouter>
+      </div>
+    )
+  }
+
+})
+
+
+let MapStateToProps = (state: Global_state_type) => {
+  return {
+    isInit: state.app.is_initialize,
+    isFetch: state.app.is_fetch,
+    currentUserID: state.app.currentUserID,
+    userPage: state.userPage,
+    isNewPost: state.userPosts.isOnNewPost,
+    isAuth: state.auth.is_auth
+  }
+}
+let MapDispatchToProps = (dispatch: any) => {
+  return {
+    init: () => {
+      dispatch(InitializeThunk())
+    },
+    setIsOnNewPost: (isPost: boolean) => {
+      dispatch(postActions.setIsOnnewPost(isPost))
+    }
+  }
 }
 
-export default App;
+export const AppContainer = connect(MapStateToProps, MapDispatchToProps)(App)
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import {App} from './App';
+import { AppContainer } from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { store } from './Redux/Store';
+import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <AppContainer/>
+    </Provider>
+
   </React.StrictMode>
 );
 
